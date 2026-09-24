44f0f1d3a3d8d6d72b07f1dff2183fc10f0dc519
Nnamdi Obi <obin@mit.edu>
2026-09-24 19:09:02 +0000

    Allowed pip building fast_knn_cu

--- fast_knn_cu/__init__.py
+++ fast_knn_cu/__init__.py
@@ -1,9 +1,9 @@
+# Gets copied to build directory using CMake.
 import torch
-from build import fast_cu_knn
-
+from .fast_cu_knn import run_knn
 
 test_tensor = torch.rand((10_250_000, 3)).cuda()
 test_tensor_2 = torch.rand((40_000_000, 3)).cuda()
 radius = 2
 # Fast!
-print(fast_cu_knn.run_knn(test_tensor, test_tensor_2, radius))
+print(run_knn(test_tensor, test_tensor_2, radius))
--- install_fast_knn_cu.bash
+++ install_fast_knn_cu.bash
@@ -1,11 +1,19 @@
 cd fast_knn_cu || return
-rm -rf build
-mkdir build
-cd build || return
-# When building directly we target a specific CUDA architecture.
+
+# If building with CMake directly
+#######
+# rm -rf build
+# mkdir build
+# cd build || return
 # CXX=$CXX CC=$CC cmake -DCMAKE_CUDA_ARCHITECTURES="50;60;61;70;75;80;86;90" ..
 # make -j8 || exit 1
+
 # Fast iteration
-CXX=$CXX CC=$CC cmake -G Ninja -DCMAKE_CUDA_ARCHITECTURES="60" -DTORCH_CUDA_ARCH_LIST="6.0" ..
-ninja -j10 || exit 1
+# CXX=$CXX CC=$CC cmake -G Ninja -DCMAKE_CUDA_ARCHITECTURES="60" -DTORCH_CUDA_ARCH_LIST="6.0" ..
+# ninja -j10 || exit 1
+#######
+
+# Pip install with a specific architecture
+export CMAKE_CUDA_ARCHITECTURES="60"
+pip install .
 cd ../..
